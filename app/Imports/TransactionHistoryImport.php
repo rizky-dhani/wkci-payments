73f095d4761f68fe41d5c8e0a8d922dbad2e2acd
Rizky Dhani <rizkydhani15@gmail.com>
2025-12-16 09:54:50 +0700

    refactor(import): simplify date parsing in TransactionHistoryImport

--- app/Imports/TransactionHistoryImport.php
+++ app/Imports/TransactionHistoryImport.php
@@ -17,21 +17,8 @@ class TransactionHistoryImport implements ToModel, WithHeadingRow
     {
         $timeConverted = Date::excelToDateTimeObject($rows['transaction_time']);
         $timeString = Carbon::parse($timeConverted)->toTimeString();
-        
-        // Parse transaction date from Excel - it could be a numeric Excel date or a string
-        $transactionDateValue = $rows['transaction_date'];
-        $dateObject = null;
-        
-        if (is_numeric($transactionDateValue)) {
-            // If it's a numeric Excel timestamp, convert it to DateTime object
-            $dateObject = Date::excelToDateTimeObject($transactionDateValue);
-        } else {
-            // If it's a string, try to parse it directly
-            $dateObject = Carbon::parse($transactionDateValue);
-        }
-
         return new TransactionHistory([
-            'transaction_date' => $dateObject,
+            'transaction_date' => Carbon::createFromFormat('Y-m-d', $rows['transaction_date']),
             'transaction_time' => Carbon::createFromFormat('H:i:s', $timeString),
             'amount' => $rows['amount'],
             'remarks' => $rows['remarks']
