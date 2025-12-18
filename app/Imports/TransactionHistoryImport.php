66fd6a670038f241621a3481c405988421f9d184
Rizky Dhani <rizkydhani15@gmail.com>
2025-12-18 11:25:41 +0700

    refactor(import): improve date conversion logic in transaction history import

--- app/Imports/TransactionHistoryImport.php
+++ app/Imports/TransactionHistoryImport.php
@@ -15,10 +15,12 @@ class TransactionHistoryImport implements ToModel, WithHeadingRow
 {
     public function model(array $rows)
     {
+        $dateConverted = Date::excelToDateTimeObject($rows['transaction_date']);
+        $dateString = Carbon::parse($dateConverted)->toDateString();
         $timeConverted = Date::excelToDateTimeObject($rows['transaction_time']);
         $timeString = Carbon::parse($timeConverted)->toTimeString();
         return new TransactionHistory([
-            'transaction_date' => Carbon::parse($rows['transaction_date'])->toDateString(),
+            'transaction_date' => Carbon::createFromFormat('Y-m-d', $dateString),
             // 'transaction_date' => Carbon::createFromFormat('Y-m-d', $rows['transaction_date']),
             'transaction_time' => Carbon::createFromFormat('H:i:s', $timeString),
             'amount' => $rows['amount'],
